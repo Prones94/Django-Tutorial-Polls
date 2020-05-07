0ddbfeccc692eb83f523cf02cee49bb388b24c57
Ian Rones <ronesp.85@gmail.com>
2020-05-07 15:53:34 -0700

    [add] new changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,13 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-<<<<<<< HEAD
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-=======
-
-# LOGIN settings
-LOGIN_REDIRECT_URL = '/polls/'
-LOGOUT_REDIRECT_URL = '/polls/'
->>>>>>> 9a3ff78b71fd8837754cc0d9ce17b2cb8cf2f424
